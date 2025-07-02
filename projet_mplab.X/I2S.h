64db503fc5b9ab6150196438f718f35e1aae2a99
JasonLLeclerc <146017982+JasonLLeclerc@users.noreply.github.com>
2025-07-02 12:48:53 -0400

    I2S
    Ajout I2S

---
+++ projet_mplab.X/I2S.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+#ifndef _UART_YBL_H    /* Guard against multiple inclusion */
+#define _UART_YBL_H
+
+/* Provide C++ Compatibility */
+#ifdef __cplusplus
+extern "C" {
+#endif   
+    
+void SPI2_I2S_Config(void);
+
+void Timer5_Config(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _UART_YBL_H */
+
+/* *****************************************************************************
+ End of File
+ */
